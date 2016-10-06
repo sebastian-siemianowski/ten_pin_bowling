8d29e02f5ea96d9810079e86ea3983d6e2bb879f
Sebastian Siemianowski <rudph2@gmail.com>
2016-10-06 05:12:42 +0100

    comments out puts

--- lib/score_calculator.rb
+++ lib/score_calculator.rb
@@ -12,26 +12,26 @@ class ScoreCalculator
       if frame.spare? && not_last_frame?(frames, index)
         spare_score = frames[(index +1)].roll_1
         result[(index + 1).to_s] = (spare_score + frame_score)
-        puts (spare_score + frame_score)
-        puts '+'
+        # puts (spare_score + frame_score)
+        # puts '+'
       elsif frame.strike? && not_last_frame?(frames, index)
         next_frame = frames[(index+1)]
 
         if next_frame.strike? && !next_frame.bonus
           following_frame = frames[(index+2)]
           result[(index + 1).to_s] = 10 + next_frame.roll_1 + next_frame.roll_2.to_i + following_frame.roll_1
-          puts 10 + next_frame.roll_1 + next_frame.roll_2.to_i + following_frame.roll_1
-          puts '+'
+          # puts 10 + next_frame.roll_1 + next_frame.roll_2.to_i + following_frame.roll_1
+          # puts '+'
         end
 
         if next_frame.bonus || next_frame.miss? || next_frame.spare?
           result[(index + 1).to_s] = 10 + next_frame.roll_1 + next_frame.roll_2
-          puts 10 + next_frame.roll_1 + next_frame.roll_2
-          puts '+'
+          # puts 10 + next_frame.roll_1 + next_frame.roll_2
+          # puts '+'
         end
       else
-        puts frame_score
-        puts '+'
+        # puts frame_score
+        # puts '+'
         unless frame.bonus
           result[(index + 1).to_s] = frame_score
         end
